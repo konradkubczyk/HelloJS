6327473d94015102b16f50c9d1312293a8ecde5f
Konrad Kubczyk <konrad@kubczyk.com>
2022-01-02 22:10:50 +0100

    New script

---
+++ sum2DArray.js
@@ -0,0 +1,29 @@
+// Sum of elements of a 2D array
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+let array = [];
+
+let xDimension = getRandomInteger(2, 4);
+let yDimension = getRandomInteger(2, 4);
+
+for (let i = 0; i < xDimension; i++) {
+    array[i] = [];
+    for (let j = 0; j < xDimension; j++) {
+        array[i][j] = getRandomInteger(0, 10);
+    }
+    console.log(array[i].join(" "));
+}
+
+let sum = 0;
+console.log('');
+
+for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+        sum += array[i][j];
+    }
+}
+
+console.log(`Sum: ${sum}`)
