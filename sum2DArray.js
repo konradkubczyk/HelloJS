66cbf3bf4a1f99f911afc7a2a49f956acf455d32
Konrad Kubczyk <konrad@kubczyk.com>
2022-05-06 19:15:41 +0200

    Minor changes

--- sum2DArray.js
+++ sum2DArray.js
@@ -6,8 +6,8 @@ function getRandomInteger(min, max) {
 
 let array = [];
 
-let xDimension = getRandomInteger(2, 7);
-let yDimension = getRandomInteger(2, 7);
+let xDimension = getRandomInteger(2, 10);
+let yDimension = getRandomInteger(2, 10);
 
 for (let i = 0; i < xDimension; i++) {
     array[i] = [];
